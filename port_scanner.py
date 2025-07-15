e29a872e24856e18c0847e7292a6ce591e4554f9
austinpinedo <138412259+austinpinedo@users.noreply.github.com>
2025-07-15 13:31:51 -0500

    Add files via upload

--- port_scanner.py
+++ port_scanner.py
@@ -1,12 +1,11 @@
 #In this lab, we are creatinga port scanner. We are going to determine if the first 500 ports are open or closed.
  
 from socket import *
+import ipaddress
 import time
  
-def port_scanner(port_num):
+def port_scanner(IP_ADDRESS, port_num):
 
-    IP_ADDRESS = ('127.0.0.1')
-    
     try:
         sock = socket(AF_INET, SOCK_STREAM)
         sock.settimeout(5)
@@ -15,16 +14,23 @@ def port_scanner(port_num):
         sock.close()
 
     except Exception as e:
-        pass
+        print(f"Port {port_num}: CLOSED ({e})")
 
 def main():
     print("Welcome to Port Scanner!")
-    print("The address scanned today is 127.0.0.1")
+    try: 
+        IP_ADDRESS = input("Enter an IP Address:")
+        ipaddress.ip_address(IP_ADDRESS)
+    except ValueError:
+        print("Invalid IP Address.")
+        return
+    
+    print(f"The address scanned today is {IP_ADDRESS}")
     print("__________________________")
 
     begin = time.time()
     for port in range(0,500):
-        port_scanner(port)
+        port_scanner(IP_ADDRESS, port)
     finish = time.time()
 
     completion = finish - begin
