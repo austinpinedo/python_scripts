e619e7077f10adba8c42770c7d7b481689b72953
austinpinedo <138412259+austinpinedo@users.noreply.github.com>
2025-07-15 13:05:02 -0500

    Add files via upload

---
+++ port_scanner_og.py
@@ -0,0 +1,43 @@
+#In this lab, we are creatinga port scanner. We are going to determine if the first 500 ports are open or closed.
+ 
+from socket import *
+import time
+ 
+
+def port_scanner(port_num):
+
+    IP_ADDRESS = ('127.0.0.1')
+
+
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.settimeout(5)
+    sock.connect(IP_ADDRESS)
+    sock.gethostbyname(IP_ADDRESS)
+
+    print(f"Port {port_num}: OPEN")
+    sock.close()
+
+
+ 
+
+def main():
+    print("Welcome to Port Scanner!")
+    print("The address scanned today is 127.0.0.1")
+    print("__________________________")
+
+ 
+
+    begin = time.time()
+    for port in range(0,500):
+        port_scanner(port)
+    finish = time.time()
+
+ 
+
+    completion = finish - begin
+    print(completion)
+ 
+if __name__ == '__main__':
+
+    main()
+
