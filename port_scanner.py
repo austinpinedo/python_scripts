fb1c76514c1eacb519e15639ae31f33d916f79f1
austinpinedo <138412259+austinpinedo@users.noreply.github.com>
2025-07-15 13:15:59 -0500

    Add files via upload

--- port_scanner.py
+++ port_scanner.py
@@ -3,31 +3,29 @@
 from socket import *
 import time
  
-
 def port_scanner(port_num):
 
-    IP_ADDRESS = input("input IP address: ")
+    IP_ADDRESS = ('127.0.0.1')
+    
+    try:
+        sock = socket(AF_INET, SOCK_STREAM)
+        sock.settimeout(5)
+        sock.connect((IP_ADDRESS,port_num))
+        print(f"Port {port_num}: OPEN")
+        sock.close()
 
-
-    sock = socket(AF_INET, SOCK_STREAM)
-    sock.settimeout(5)
-    sock.connect(IP_ADDRESS)
-    sock.gethostbyname(IP_ADDRESS)
-
-    print(f"Port {port_num}: OPEN")
-    sock.close()
-
-
- 
+    except Exception as e:
+        pass
 
 def main():
+    print("Welcome to Port Scanner!")
+    print("The address scanned today is 127.0.0.1")
+    print("__________________________")
 
     begin = time.time()
     for port in range(0,500):
         port_scanner(port)
     finish = time.time()
-
- 
 
     completion = finish - begin
     print(completion)
